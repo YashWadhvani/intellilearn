51f9cd810beef09b39585c3fdc3b4264734e21a5
YashWadhvani <yashwadhvani123@gmail.com>
2024-08-26 14:16:26 +0530

    Menu Started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-
+      <Header />
+      <h1>Hello</h1>
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import Logo from "../assets/logo.png";
+import "../styles/Header.scss";
+
+function Header() {
+  return (
+    <>
+      <Navbar
+        className="bg-body-dark mb-3"
+        bg="dark"
+        expand="false"
+        data-bs-theme="dark"
+      >
+        <Container fluid>
+          <Navbar.Brand href="/">
+            <img src={Logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-false`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-false`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-false`}
+            placement="end"
+            data-bs-theme="dark"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-false`}>
+                Menu
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/courses">Courses</Nav.Link>
+                <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Header;
